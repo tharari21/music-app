cd909f558abefaa1c3cb05fc0cd2799824a94fa3
Tomer Harari <tharari21@gmail.com>
2024-12-14 23:17:47 -0500

    wip

--- src/Song.jsx
+++ src/Song.jsx
@@ -1,15 +1,17 @@
-// props
 import PlayButton from "./assets/playbutton.png";
+// props
 const Song = ({ song }) => {
+  console.log(song);
   return (
     <li className="h-16 my-2 border border-blue-300 shadow-md">
       <div className="w-[100%] flex justify-between">
         <h3 className="pl-3 pt-2 font-bold text-lg">{song.name}</h3>
-        <div>
+        <div className="pr-5">
           <img className="w-8 pt-4" src={PlayButton} />
         </div>
       </div>
     </li>
   );
 };
+
 export default Song;
--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,5 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+const spotify_client_secret = "TOMER";
+createRoot(document.getElementById("root")).render(<App />);
